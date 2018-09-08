a0b783c305473990ca1a3ac410e76ed7f30b63c1
sentu <sentu@DESKTOP-4DU45R6>
2018-09-08 20:57:23 +0300

    Add project files.

---
+++ Narail/Controllers/BlogController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Narail.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Narail | Blog";
+            return View();
+        }
+        public ActionResult Category()
+        {
+            return View();
+        }
+        public ActionResult Populer()
+        {
+            return View();
+        }
+        public ActionResult Tags()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
